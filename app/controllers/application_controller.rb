250c752469dd2517dbfd7ae67adab2c89cff0897
Natalia Poletaeva <natfly799@gmail.com>
2022-05-11 21:14:19 +0500

    Fix linters

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   add_flash_types :info, :error, :warning
-  
+
   def current_user
     User.first
   end
--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -12,14 +12,14 @@ RSpec.describe 'Users', type: :request do
     end
   end
 
-    describe 'GET single user' do
-      before(:each) do
-        it 'has a 200 success status code' do
-          expect(response).to have_http_status(200)
-        end
-        it 'If a correct template was rendered' do
-          expect(response).to render_template(:show)
-        end
+  describe 'GET single user' do
+    before(:each) do
+      it 'has a 200 success status code' do
+        expect(response).to have_http_status(200)
+      end
+      it 'If a correct template was rendered' do
+        expect(response).to render_template(:show)
+      end
     end
   end
 end
